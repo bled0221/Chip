9fb617372216fa4fcaf27c9a03cf280c6f4794f1
bled0221 <firefox8979@gmail.com>
2026-05-30 15:23:53 +0900

    Design: 다크/라이트 공용 만능 회색(#72767d) 적용

--- commands/backbite.js
+++ commands/backbite.js
@@ -46,7 +46,7 @@ module.exports = {
 
         // 익명 박스(임베드) 꾸미기
         const anonymousEmbed = new EmbedBuilder()
-            .setColor(1)
+            .setColor('#72767d')
             .setTitle('익명 뒷담 메시지')
             .setDescription(`"${content}"`)
             .setTimestamp()
